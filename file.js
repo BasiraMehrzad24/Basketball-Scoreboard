c503370d1d268ed09887128f16c164a2e37035e3
BasiraMehrzad24 <basiramehrzad25@gmail.com>
2024-02-07 16:26:19 +0430

    project done

---
+++ file.js
@@ -0,0 +1,54 @@
+//let plus = document.getElementById("one")
+let countEl = document.getElementById("count-el")
+let disCount = document.getElementById("discount")
+
+let count = 12
+let discount=5
+
+function increment1() {
+    if (count <= 12) {
+        count++;
+        countEl.textContent=count
+    }
+}
+
+function increment2() {
+    if (count <= 12) {
+        count+=2
+        countEl.textContent=count
+    }
+}
+
+
+function increment3() {
+    if (count <= 12) {
+        count+=3
+        countEl.textContent=count
+    }
+}
+
+
+
+function decrement1(){
+    if (discount = 5) {
+        discount--;
+        disCount.textContent=discount
+    }
+
+}
+
+
+function decrement2(){
+    if (discount = 5) {
+        discount-=2
+        disCount.textContent=discount
+    }
+
+}
+
+function decrement3(){
+    if (discount = 5) {
+        discount-=3
+        disCount.textContent=discount
+    }
+}
