3262d259c7a93c090e8541ade95066bc72c3accc
BasiraMehrzad24 <basiramehrzad25@gmail.com>
2024-02-12 15:42:50 +0430

    Fix issue

--- file.js
+++ file.js
@@ -1,4 +1,4 @@
-//let plus = document.getElementById("one")
+
 let countEl = document.getElementById("count-el")
 let disCount = document.getElementById("discount")
 
@@ -6,26 +6,30 @@ let count = 12
 let discount=5
 
 function increment1() {
-    if (count <= 12) {
+    if (count = 12) {
         count++;
         countEl.textContent=count
     }
 }
 
+
 function increment2() {
-    if (count <= 12) {
-        count+=2
+    if (count = 12) {
+        count += 2
         countEl.textContent=count
     }
 }
 
 
 function increment3() {
-    if (count <= 12) {
-        count+=3
+    if (count = 12) {
+        count += 3
         countEl.textContent=count
     }
 }
+
+
+
 
 
 
@@ -40,15 +44,16 @@ function decrement1(){
 
 function decrement2(){
     if (discount = 5) {
-        discount-=2
+        discount -= 2
         disCount.textContent=discount
     }
 
 }
 
+
 function decrement3(){
     if (discount = 5) {
-        discount-=3
+        discount -= 3
         disCount.textContent=discount
     }
 }
